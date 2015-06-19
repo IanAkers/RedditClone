b7be0a29004c0a0f933b87b71e0cd2d1c3b589fe
Ian <ian.asher.akers@gmail.com>
2015-06-19 11:49:52 -0400

    Create login, logout functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  attr_reader: :password
-  validates :username, :password_digest, presence: true, uniqueness: true
-  validates :password, length: {minimum: 6, allow_nil: true}
+  attr_reader :password
+  validates :username, :password_digest, :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
 
   def password=(password)
@@ -9,14 +9,14 @@ class User < ActiveRecord::Base
     self.password_digest = BCrypt::Password.create(password)
   end
 
-  def self.validate_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     @user = User.find_by(username: username)
     return nil if @user.nil?
     @user.is_password?(password) ? @user : nil
   end
 
-  def reset_session_token
-    self.session_token = RandomSecure.urlsafe_base64(16)
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
     self.save!
     self.session_token
   end
@@ -28,6 +28,6 @@ class User < ActiveRecord::Base
   private
 
   def ensure_session_token
-    self.session_token ||= RandomSecure.urlsafe_base64(16)
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
   end
 end
